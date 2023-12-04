45f89297581b063003573e718f5d813ad9a896df
Anum <anumshahwar01@gmail.com>
2023-12-04 17:07:48 +0000

    linux cluster monitoring

--- host_info.sh
+++ host_info.sh
@@ -12,7 +12,7 @@ fi
 hostname=$(hostname -f)
 lscpu_out=`lscpu`
 cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
-Architecture= $(echo $lscpu_out | egrep ^Architecture: | awk {print $2} | xargs)
+Architecture= $(echo $lscpu_out | egrep ^Architecture: | awk '{print $2}' | xargs)
 cpu_model= $(echo "$lscpu_out" | egrep "^Model:" | awk '{print $2}' | xargs)
 cpu_mhz= $(echo "$lscpu_out" | egrep "^CPU\sMHz:" | awk '{print $3}' | xargs)
 l2_cache= $(echo "$lscpu_out" | egrep "^L2\scache:" | awk '{print $3}' | xargs)
--- host_usage.sh
+++ host_usage.sh
@@ -11,19 +11,23 @@ if [ "$#" -ne 5 ]; then
     echo "Illegal number of parameters"
     exit 1
 fi
+date
+
 
 vmstat_mb=$(vmstat --unit M)
 hostname=$(hostname -f)
 
-memory_free=$(echo "$vmstat_mb" | awk '{print $4}'| tail -n1 | xargs)
+memory_free=$vmstat --unit M | awk '{print $4}'| tail -n1 | xargs
 disk_io=$vmstat --unit M | awk '{print $10}' | tail -n1 | xargs
-disk_available=(df -BM / ...
+disk_available=(df -BM / ...)
 cpu_idle=$vmstat --unit M | awk '{print $15}' | tail -n1 | xargs
 cpu_kernel=$vmstat --unit M | awk '{print $14}' | tail -n1 | xargs
 timestamp=$(vmstat -t | awk'{print $18}' | tail -n1 | xargs
 
 host_id="(SELECT id FROM host_info WHERE hostname='$hostname')";
 
+insert_stmt=" INSERT INTO host_usage (host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available, timestamp) VALUES ('$1', $2099, $63, $27, $12, $21130, '2023-11-29 23:59:39');"
 
-insert_stmt="INSERT INTO host_usage (host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available, timestamp) VALUES ('$1', $2099, $63, $27, $12, $21130, '2023-11-29 23:59')
+
+
 
